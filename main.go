93c6cc572be4db8de8c456e0858de1ad86a1e763
Alim Aldybergenov <alimaldybergenov@Alims-MacBook-Pro.local>
2022-10-12 11:45:34 +0600

    setting up router

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func main() {
+
+	log.Println("Starting the app")
+
+	router := mux.NewRouter()
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	router.HandleFunc()
+	router.HandleFunc()
+
+}
